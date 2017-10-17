409a18c497151017122d792093f026d4907f979b
Kinley Ramson <kinleyramson@gmail.com>
2017-10-17 16:23:04 -0700

    the most beautiful dunder print ever

--- series.py
+++ series.py
@@ -32,10 +32,36 @@ def sum_series(n, op1=0, op2=1):
     elif op1 == 2 and op2 == 1:
         return lucas(n)
     else:
+        if n < 0:
+            return False
         if n == 0:
             return op1
         elif n == 1:
             return op2
         else:
-            print(sum_series(n - 1, op1, op2) + sum_series(n - 2, op1, op2))
             return sum_series(n - 1, op1, op2) + sum_series(n - 2, op1, op2)
+
+
+if __name__ == "__main__":
+    print('This module contains three functions.')
+    print()
+    print('fibonacci(n):')
+    print('    Returns the nth number of fibonacci sequence.')
+    print()
+    print('>>> fibonacci(2)')
+    print(fibonacci(2))
+    print()
+    print('lucas(n)')
+    print('    Returns the nth number in the lucas sequence.')
+    print()
+    print('>>> lucas(3)')
+    print(lucas(3))
+    print()
+    print('sum_series(n, op1, op2):')
+    print('    Returns the nth number of')
+    print('    a fibonacci-like sequence, with the optional parameters')
+    print('    as starting values; the default starting values will')
+    print('    produce a fibonacci sequence.')
+    print()
+    print('>>> sum_series(0, 5, 4)')
+    print(sum_series(0, 5, 4))
