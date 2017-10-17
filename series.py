994498c714739e1ecceb2a1ef0e9cf6c0fe52c8f
CHELSEA DOLE <chelseadole@gmail.com>
2017-10-17 14:32:02 -0700

    finishing lucas function in series.py module

--- series.py
+++ series.py
@@ -8,7 +8,17 @@ def fibonacci(n):
     elif n == 1 or n == 2:
         return 1
     elif n > 2:
-        print(fibonacci(n - 1) + fibonacci(n - 2))
         return fibonacci(n - 1) + fibonacci(n - 2)
+    else:
+        return False
+
+def lucas(n):
+    """Give the nth number in the lucas series when given n."""
+    if n == 0:
+        return 2
+    elif n == 1:
+        return 1
+    elif n > 1:
+        return lucas(n - 1) + lucas(n - 2)
     else:
         return False
